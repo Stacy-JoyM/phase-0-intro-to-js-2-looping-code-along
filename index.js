52a023bd19fe9d890010c919707f6715ff4f954b
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-08 12:06:10 +0300

    Loops

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+let array = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(array){
+    let messages = []
+    for (let i = 0; i < array.length; i++){
+        messages.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+writeCards(array);
+
+function countDown(){
+    for (let i=0; i < 11; i++){
+        console.log(i);
+    }
+}
+countDown()
